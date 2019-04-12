cf592a269f97a5777210b20b53e57bc6f2f8b778
Robert Xu <robertpingxu@gmail.com>
2019-04-12 04:18:51 -0400

    Adding back in modals

--- ACQ-Proto/UI/js/components/formWrapper.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="well bs-component" style="margin: 0 auto; width: 85%; max-width: 1400px;">
-        <form class="form-horizontal" onsubmit="return false;">
-            <fieldset>
-                <slot></slot>
-            </fieldset>
-        </form>
-    </div>
-</template>
---
+++ ACQ-Proto/UI/js/components/modal.vue
@@ -0,0 +1,42 @@
+<template>
+    <div :style="styleObj">
+        <div class="modal-dialog" style="position: fixed;top: 30%;left: 50%;transform: translate(-50%, -50%);">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" style="color: #222222;" aria-hidden="true"
+                            @click="close">&times;</button>
+                    <h4 class="modal-title">
+                        <slot name="title">Alert</slot>
+                    </h4>
+                </div>
+                <div class="modal-body">
+                    <slot name="body"></slot>
+                </div>
+                <div class="modal-footer">
+                    <slot name="footer">
+                        <button type="button" class="btn btn-default" @click="close" autofocus>Close</button>
+                    </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["close"],
+        data: function () {
+            return {
+                styleObj: {
+                    position: "fixed",
+                    left: "0px",
+                    top: "0px",
+                    width: "100%",
+                    height: "100%",
+                    zIndex: 1000,
+                    backgroundImage: "url(img/backdrop.png)"
+                }
+            };
+        }
+    };
+</script>
